fe8ac452ca770ed6a92574fd65f94998288b1b57
DanimusPrime <30447835+danimusprime@users.noreply.github.com>
2018-08-07 12:30:43 -0500

    Update items.py

--- scaperapp/items.py
+++ scaperapp/items.py
@@ -1,1 +1,14 @@
-
+>>> from scrapy.item import Item, Field
+>>> from items import LivingSocialDeal
+>>> deal = LivingSocialDeal(title="$20 off yoga classes", price="50")
+>>> print deal
+LivingSocialDeal(title='$20 off yoga classes', price='50')
+>>> deal['title']
+'XXXXXXXXXXXXXXXXXXXX'
+>>> deal.get('title')
+'XXXXXXXXXXXXXXXXXXXX'
+>>> deal['price']
+'50'
+>>> deal['location'] = "New York"
+>>> deal['location']
+'New York'
