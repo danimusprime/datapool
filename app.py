d16a778eac22cf21903b13e1b7f4d41c49f5537b
danimusprime <30447835+danimusprime@users.noreply.github.com>
2018-09-16 11:34:29 -0500

    changed access_token to access_token_key in auth.set...

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ access_token_secret = "process.env.access_token_secret"
 
 # Twitter initialization
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth.set_access_token(access_token_key, access_token_secret)
 api = tweepy.API(auth)
 
 # Postgresql initialization
