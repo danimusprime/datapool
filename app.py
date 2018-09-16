bd6f778e6025805c06106ac8b3465554405ff21b
danimusprime <30447835+danimusprime@users.noreply.github.com>
2018-09-16 13:15:42 -0500

    database from DATABASE_URL

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ api = tweepy.API(auth)
 # Postgresql initialization
 connection = psycopg2.connect(
     host= "Host",
-    database= "DATABASE_URL",
+    database= "Database",
     port= "Port",
     user= "User",
     password= "Password")
