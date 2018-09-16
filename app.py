03789438262c39bb4ca8d33d9f0ab4e9e9f605e5
danimusprime <30447835+danimusprime@users.noreply.github.com>
2018-09-16 16:04:20 -0500

    removed key reference

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ consumer_secret = "process.env.consumer_secret",
 access_token_key = "process.env.access_token_key",
 access_token_secret = "process.env.access_token_secret",
 password = "process.env.Password",
-user = "process.env.User",
-host = "process.env.Host"
+user = "process.env.User"
 
 # Twitter initialization
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
