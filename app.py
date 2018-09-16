3d17eb61145ad248428030fcdf9f0f2c647fa9ea
danimusprime <30447835+danimusprime@users.noreply.github.com>
2018-09-16 00:24:23 -0500

    Uppercased API

--- app.py
+++ app.py
@@ -29,15 +29,15 @@ connection = psycopg2.connect(
 cursor = connection.cursor()
 
 #CREATE TABLE tweets (id SERIAL PRIMARY KEY, tweet_id BIGINT NOT NULL, text VARCHAR NOT NULL, screen_name VARCHAR NOT NULL, author_id INTEGER, created_at VARCHAR NOT NULL, inserted_at TIMESTAMP NOT NULL)
-
 try:
-    statuses = api.user_timeline('124297267')  #14903018 batenkaitos
+    statuses = API.user_timeline('124297267')  #14903018 batenkaitos
     for s in statuses:
         # To remove duplicate entries
         # See http://initd.org/psycopg/docs/faq.html for "not all arguments converted during string formatting"
         cursor.execute("SELECT id FROM tweets WHERE text = %s;", [s.text])
         if cursor.rowcount == 0:
-            cursor.execute("INSERT INTO tweets (tweet_id, text, screen_name, author_id, created_at, inserted_at) VALUES (%s, %s, %s, %s, %s, current_timestamp);", (s.id, s.text, s.author.screen_name, s.author.id, s.created_at))
+            cursor.execute(
+                "INSERT INTO tweets (tweet_id, text, screen_name, author_id, created_at, inserted_at) VALUES (%s, %s, %s, %s, %s, current_timestamp);", (s.id, s.text, s.author.screen_name, s.author.id, s.created_at))
             connection.commit()
 except tweepy.error.TweepError:
     print "Whoops, could not fetch news!"
