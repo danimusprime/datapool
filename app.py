08c8fffa7b148eb8eb41b8148670fde6305a2e8b
danimusprime <30447835+danimusprime@users.noreply.github.com>
2018-09-16 10:59:19 -0500

    removed indents from #keys and #twitter Inits

--- app.py
+++ app.py
@@ -5,33 +5,33 @@ import psycopg2
 # psycopg2 API doc here: http://initd.org/psycopg/docs/
 
 # Keys
-  consumer_key = "process.env.consumer_key",
-  consumer_secret: "process.env.consumer_secret",
-  access_token_key: "process.env.access_token_key",
-  access_token_secret: "process.env.access_token_secret"
+ consumer_key = "process.env.consumer_key",
+ consumer_secret: "process.env.consumer_secret",
+ access_token_key: "process.env.access_token_key",
+ access_token_secret: "process.env.access_token_secret"
 
 # Twitter initialization
-  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-  auth.set_access_token(access_token, access_token_secret)
-  api = tweepy.API(auth)
+ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+ auth.set_access_token(access_token, access_token_secret)
+ api = tweepy.API(auth)
 
 # Postgresql initialization
 connection = psycopg2.connect(
-        database= 'process.env.DATABASE'
-        host= 'process.env.HOST'
-        port= 'process.env.PORT'
-        user= 'process.env.USER'
-        password= 'process.env.PASSWORD')
-            print 'status is: ' + str(connection.status)
-                except:
-            print "unable to connect"
+    database= 'process.env.DATABASE'
+    host= 'process.env.HOST'
+    port= 'process.env.PORT'
+    user= 'process.env.USER'
+    password= 'process.env.PASSWORD')
+    print 'status is: ' + str(connection.status)
+    except:
+        print "unable to connect"
 
 cursor = connection.cursor()
 
 #CREATE TABLE tweets (id SERIAL PRIMARY KEY, tweet_id BIGINT NOT NULL, text VARCHAR NOT NULL, screen_name VARCHAR NOT NULL, author_id INTEGER, created_at VARCHAR NOT NULL, inserted_at TIMESTAMP NOT NULL)
 
 try:
-    statuses = API.list_timeline(api.me().screen_name, 'National Parks') 
+    statuses = API.list_timeline(api.me().screen_name, 'National Parks')
     for s in statuses:
         # To remove duplicate entries
         # See http://initd.org/psycopg/docs/faq.html for "not all arguments converted during string formatting"
