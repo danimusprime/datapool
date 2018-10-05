3df894b07d863518db5ed22fa4f379c3a929a67f
danimusprime <30447835+danimusprime@users.noreply.github.com>
2018-10-04 22:18:45 -0500

    json

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from tweepy import Cursor
 import psycopg2 as pg2
 import os
 from pprint import pprint
-import json
 import credentials
 
 
@@ -24,7 +23,7 @@ class TwitterClient():
     def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
-
+        print('Authenticated.')
         self.twitter_user = twitter_user
 
     def get_user_timeline_tweets(self, num_tweets):
@@ -36,18 +35,17 @@ class TwitterClient():
     def get_friend_list(self, num_friends):
         friend_list = []
         for friend in Cursor(self.twitter_client.friends).items(num_friends):
-            friend_list.append(tweet)
+            friend_list.append(friend_list)
         return friend_list
 
+
 # this class will authenticate twitter
-
-
 class TwitterAuthenticator():
     def authenticate_twitter_app(self):
         auth = OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
         auth.set_access_token(credentials.access_token_key, credentials.access_token_secret)
         return auth
-        print('Authenticated')
+        pprint('Authenticated')
 
 
 class twitter_streamer():
@@ -77,9 +75,9 @@ class twitter_listener(StreamListener):
 
     def on_data(self, data):
         try:
-            print(data)
-            with open(self.fetched_tweets_filename, 'w') as write_file:
-                json.dump(data, write_file)
+            # print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
             return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
@@ -103,13 +101,23 @@ class DatabaseConnection:
             # --> to be used when Heroku is involved (DATABASE_URL, sslmode='require')
 
     def create_table(self):
-        create_table_command = "CREATE TABLE twitter(id SERIAL PRIMARY KEY, tweet_id BIGINT NOT NULL, text_  VARCHAR NOT NULL, screen_name VARCHAR NOT NULL, author_id INTEGER, created_at VARCHAR NOT NULL, inserted_at TIMESTAMP NOT NULL)"
+        create_table_command = "CREATE TABLE twitter(
+            id SERIAL PRIMARY KEY,
+            tweet_id BIGINT NOT NULL,
+            text_  VARCHAR NOT NULL,
+            screen_name VARCHAR NOT NULL,
+            source VARCHAR NOT NULL,
+            author_id INTEGER,
+            created_at VARCHAR NOT NULL,
+            inserted_at TIMESTAMP NOT NULL)"
         self.cursor.execute(create_table_command)
         pprint('Table Created')
 
     def insert_new_record(self):
         try:
-            new_record = TwitterClient("Batenkaitos").get_user_timeline_tweets(6)
+            with tweets.json(doc) as (values('[
+
+            ]'))
             insert_command = 'INSERT INTO twitter(id, tweet_id, text, screen_name, author_id,  created_at, inserted_at) VALUES ( % s, % s, % s, % s, % s, current_timestamp)'
             self.cursor.execute(insert_command, (tweet_id, screen_name, created_at, text))
             self.cursor.commit()
@@ -128,10 +136,9 @@ if __name__ == "__main__":
     fetched_tweets_filename = "tweets.json"
 
     database_connection = DatabaseConnection()
-    twitterlistener = twitter_listener(StreamListener).on_data(data)
     # CreateTable = database_connection.create_table()
-    # insert_record = database_connection.insert_new_record()
+    insert_record = database_connection.insert_new_record()
     # twitter_client = TwitterClient('Batenkaitos')
     # print(twitter_client.get_user_timeline_tweets(6))
-    # streamer = twitter_streamer()
+    streamer = twitter_streamer().stream_tweets(fetched_tweets_filename, hash_tag_list)
     # streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
